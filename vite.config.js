63fab8d77f41f3aa622ee38b70ed8039167ebc2e
Yash Bansal <byash8587@gmail.com>
2026-04-22 20:15:08 +0530

    fix: dynamic base path for vercel and gh-pages

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,20 @@
 // // https://vite.dev/config/
 // export default defineConfig({
 //   plugins: [react()],
+// // })
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// export default defineConfig({
+//   plugins: [react()],
+//   base: "/Food-delivery-frontend/"
 // })
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: "/Food-delivery-frontend/"
+  // Agar GitHub Actions/Pages build hai toh repo name, warna Vercel/Local ke liye "/"
+  base: process.env.GITHUB_ACTIONS ? "/Food-delivery-frontend/" : "/",
 })
